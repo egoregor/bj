da1d25125a05594b9cb8234a0181df00361f253b
estarostin <estarostin@ronasit.com>
2017-01-30 15:35:28 +0600

    animations, card sprites

--- app/scripts/controllers/blackjackCtrl.js
+++ app/scripts/controllers/blackjackCtrl.js
@@ -8,26 +8,41 @@
   function blackjack($scope, $rootScope, $state, $touch, $window, $timeout) {
     var vm = this;
 
-    vm.aiCards = [{}];
-    vm.playerCards = [{}, {}];
+    vm.aiCards = [{suit: 'd_a'},{}];
+    vm.playerCards = [{suit: 'd_a'}, {suit: 'd_a'}];
 
 
     angular.element($window).bind('resize', function(){
-        angular.element(".cardInContainer").width(angular.element(".cardInContainer img").width() + 'px');
+        angular.element(".cardInContainer .cardImage").width(angular.element(".cardInContainer").height()*5/7 + 'px');
         angular.element(".tableBgWrap").width(angular.element(".tableBgWrap .tableBgImg").width() + 'px');
         angular.element(".tableBgWrap").css('marginLeft', '-' + angular.element(".tableBgWrap .tableBgImg").width()/2 + 'px');
      });
     $timeout(function () {
-        angular.element(".cardInContainer").width(angular.element(".cardInContainer img").width() + 'px');
+        angular.element(".cardInContainer .cardImage").width(angular.element(".cardInContainer").height()*5/7 + 'px');
         angular.element(".tableBgWrap").width(angular.element(".tableBgWrap .tableBgImg").width() + 'px');
         angular.element(".tableBgWrap").css('marginLeft', '-' + angular.element(".tableBgWrap .tableBgImg").width()/2 + 'px');
         angular.element(".tableWrap").css('opacity', 1);
     }, 1000);
 
-    vm.createNewCard = function() {
-      vm.aiCards.push({});
-      vm.playerCards.push({});
+    vm.createNewCardPlayer = function() {
+      vm.playerCards.push({suit: 'd_a'});
     }
+    vm.createNewCardAi = function(card) {
+      vm.aiCards.push(card);
+    }
+    vm.newCardsAi = [{suit: 'd_a'},{suit: 'd_a'},{suit: 'd_a'}];
+    vm.aiCardsAll = function(){
+      vm.aiCards[1].suit = 'd_a';
+      $('.cardInContainer:nth-child(2)').find(".cardImage").addClass(vm.aiCards[1].suit);
+      if(vm.newCardsAi.length > 0) {
+        vm.createNewCardAi(vm.newCardsAi[0]);
+        $timeout( function(){
+        }, 200 ).then( function() {
+          vm.newCardsAi.splice(0, 1);
+          vm.aiCardsAll();
+        });
+      }
+    };
     vm.removeAllCards = function(){
 
     }
--- app/scripts/directives/card.js
+++ app/scripts/directives/card.js
@@ -6,29 +6,82 @@ angular
 function card() {
   var directive = {
     restrict: 'EA',
-    template: '<img src="images/cards/ace.png" alt="" />',
+    template: '<div class="cardImage"></div>',
     scope: {
     	index: '=',
-    	type: '='
+    	type: '=',
+      suit: '='
     },
     link: function(scope, elem, attr) {
-    	console.log(scope.type);
     	var tempCardStyle = 'style="top:' + $('.deckBoxWrap').offset().top + 'px; left:' + $('.deckBoxWrap').offset().left + 'px; height:' + $('.deckBoxWrap').height() + 'px;"';
-		var tempCard = '<img src="images/cards/ace.png" class="createdCardTemp createdCardTemp' + scope.index + '" ' + tempCardStyle + '>';
-		$(".table").append(tempCard);
-		console.log();
-		$(".createdCardTemp" + scope.index).animate({
-		  height: $(elem).height() + 'px',
-		  top: $(elem).offset().top + 'px',
-		  left: $(elem).offset().left + 'px',
-		  opacity: 1
-		}, {
-		    duration: 250,
-		    complete: function( now, fx ){
-		      $(".createdCardTemp" + scope.index).remove();
-		      $(elem).css('opacity', 1);
-		    }
-		});
+  		var tempCard = '<img src="images/cardBack.png" class="createdCardTemp createdCardTemp' + scope.index + '" ' + tempCardStyle + '>';
+      
+      var overlaping = function(){
+        if((scope.index + 1) > 2) {
+          setTimeout(function(){
+            $('.' + scope.type + 'CardContainer .cardInContainer')
+            .not('.' + scope.type + 'CardContainer .cardInContainer:last-child')
+            .width($(elem).height()/100*20);
+            $(elem).last().find('.cardImage').width($(elem).height()*5/7);
+          }, 0);
+        }
+        else {
+          $(elem).find('.cardImage').width($(elem).height()*5/7);
+        }
+      };
+      overlaping();
+      $(window).resize(function() {
+        overlaping();
+      });
+
+      setTimeout(function(){
+        $(".table").append(tempCard);
+        $(".createdCardTemp" + scope.index).animate({
+          height: $(elem).height() + 'px',
+          top: $(elem).offset().top + 'px',
+          left: $(elem).offset().left + 'px',
+          opacity: 1
+        }, {
+          duration: 200,
+          easing: 'linear',
+          complete: function(){
+            $(elem).css({
+              opacity: 1
+            });
+            $(elem).find(".cardImage").addClass(scope.suit);
+            
+            $(".createdCardTemp" + scope.index).remove();
+              // $(this).animate({
+              //   borderSpacing: -90
+              // }, {
+              //   duration: 100,
+              //   easing: 'linear',
+              //   step: function(now,fx) {
+              //     console.log(now);
+              //     $(this).css('-webkit-transform','rotateY('+now+'deg)'); 
+              //   },
+              //   complete: function(){
+              //     $(this).remove();
+              //     $(elem).css({
+              //       opacity: 1,
+              //       transform: 'rotateY(90deg)',
+              //       borderSpacing: -90
+              //     })
+              //     .animate({
+              //       borderSpacing: 0
+              //     }, {
+              //       duration: 100,
+              //       easing: 'linear',
+              //       step: function(now,fx) {
+              //         console.log(now);
+              //         $(this).css('-webkit-transform','rotateY('+now+'deg)'); 
+              //       },
+              //     });
+              //   }
+              // })
+          }
+        });
+      }, 2);
     },
     controller: Card
   }
